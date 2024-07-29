3743774a5cd075c0570daa5eb9425b77f903daaf
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-07-29 13:28:20 -0300

    Rota: Detalhes do convite

---
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -0,0 +1,72 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "@/lib/prisma";
+import { rolesSchema } from "@sac/authorization";
+import { BadRequestError } from "@/http/errors/bad-request";
+
+export async function getInvite(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .get('/invites/:inviteId', {
+            schema: {
+                tags: ['Invites'],
+                summary: 'Get an invite',
+                params: z.object({
+                    inviteId: z.string().uuid()
+                }),
+                response: {
+                    200: z.object({
+                        invite: z.object({
+                            id: z.string().uuid(),
+                            email: z.string().email(),
+                            role: rolesSchema,
+                            createdAt: z.date(),
+                            author: z.object({
+                                id: z.string().uuid(),
+                                name: z.string().nullable(),
+                                avatarUrl: z.string().url().nullable()
+                            }),
+                            organization: z.object({
+                                name: z.string()
+                            })
+                        })
+                    })
+                }
+            }
+        }, async (request, reply) => {
+            const { inviteId } = request.params
+
+            const invite = await prisma.invite.findUnique({
+                select: {
+                    id: true,
+                    email: true,
+                    role: true,
+                    createdAt: true,
+                    author: {
+                        select: {
+                            id: true,
+                            name: true,
+                            avatarUrl: true
+                        }
+
+                    },
+                    organization: {
+                        select: {
+                            name: true
+                        }
+                    }
+                },
+                where: {
+                    id: inviteId
+                }
+            })
+
+            if (invite == null) {
+                throw new BadRequestError('Invite not found')
+            }
+
+            return reply.status(200).send({
+                invite
+            })
+        })
+}
