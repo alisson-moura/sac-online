2baac59c2f269cdc75cfc042010ecf75ab25cf61
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-07-29 13:58:55 -0300

    Rota: Aceitar convite

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "@/lib/prisma";
+import { BadRequestError } from "@/http/errors/bad-request";
+import { authMiddleware } from "@/http/middlewares/auth";
+
+export async function acceptInvite(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .register(authMiddleware)
+        .post('/invites/:inviteId/accept', {
+            schema: {
+                tags: ['Invites'],
+                security: [{ bearerAuth: [] }],
+                summary: 'Accept an invite',
+                params: z.object({
+                    inviteId: z.string().uuid()
+                }),
+                response: {
+                    204: z.null()
+                }
+            }
+        }, async (request, reply) => {
+            const userId = await request.getCurrentUserId()
+            const { inviteId } = request.params
+
+
+            const invite = await prisma.invite.findUnique({
+                where: {
+                    id: inviteId
+                }
+            })
+            if (invite == null) {
+                throw new BadRequestError('Invite not found')
+            }
+
+            const user = await prisma.user.findUniqueOrThrow({ where: { id: userId } })
+            if (user.email != invite.email) {
+                throw new BadRequestError('This invite belongs to another user.')
+            }
+
+            await prisma.$transaction([
+                prisma.member.create({
+                    data: {
+                        userId,
+                        organizationId: invite.organizationId,
+                        role: invite.role
+                    }
+                }),
+                prisma.invite.delete({
+                    where: {
+                        id: invite.id
+                    }
+                })
+            ])
+
+            return reply.status(204).send()
+        })
+}
