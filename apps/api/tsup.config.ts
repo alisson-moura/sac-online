eeed78d4e298e6d849af19c1943dbb3f610d559c
alisson-moura <alisson.mo.moura@outlook.com.br>
2024-10-31 14:05:52 -0300

    chore: add build and start scripts

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,12 @@
+import {defineConfig} from 'tsup'
+
+export default defineConfig({
+    entry: ['src'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    noExternal: [
+        "@sac/authorization",
+        "@sac/env"
+    ]
+})
