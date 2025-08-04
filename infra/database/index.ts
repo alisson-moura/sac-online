ac633bf2e9f0deda105225cf6e5a0ae693763018
Alisson Matheus de Oliveira Moura <alisson.mo.moura@outlook.com.br>
2025-08-04 18:33:01 +0000

    fix: adiciona o ssl nas configurações

--- infra/database/index.ts
+++ infra/database/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@/infra/database/schema";
 import { Pool } from "pg";
-import { env } from "@/lib/env";
+import { env, isDev, isTest } from "@/lib/env";
 
 export const pool = new Pool({
   host: env.POSTGRES_HOST,
@@ -10,6 +10,7 @@ export const pool = new Pool({
   user: env.POSTGRES_USER,
   password: env.POSTGRES_PASSWORD,
   database: env.POSTGRES_DB,
+  ssl: (isDev || isTest) ? false : true,
   options: `-c search_path=${env.POSTGRES_SCHEMA}`
 });
 
